dc0debcfe2f4b1cc496e99b89e52741ce9ea29a1
Omry Ben David <omryb@mellanox.com>
2020-04-01 17:14:44 +0300

    The code we wrote at class with Shor Gadi for Project with c#
    Signed-off-by: Omry Ben David <omryb@mellanox.com>

---
+++ Project_c#/DbConnector/DAL/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Tel { get; set; }
+
+        public Student(string name, string family, string tel)
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Name = name;
+            this.Family = family;
+            this.Tel = tel;
+
+        }
+
+        public void PrintTo()
+        {
+            string str = "ID: " + Id + "Name: " + Name + "Family: " + Family + "Tel: " + Tel;
+            Console.WriteLine(str);
+        }
+
+    }
+}
---
+++ Project_c#/DbConnector/DbConnector/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+
+namespace DbConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello DbConnector");
+            DAL.Student s1 = new DAL.Student("Moshe ", "Cohen ", "0123 ");
+            s1.PrintTo();
+            StudentDB studentdb = new StudentDB();
+            Console.WriteLine(studentdb.AddNewStudent(s1));
+            Console.ReadKey();
+        }
+    }
+}
